33bfb1e48fc781cc30de435d806512497ed2a0d2
Dai1678 <dai7774@gmail.com>
2020-03-31 13:02:12 +0900

    CardenActivityTestを作成

---
+++ app/src/androidTest/java/com/google/samples/apps/sunflower/GardenActivityTest.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2020 TOYAMA Sumio <jun.nama@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class GardenActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(GardenActivity::class.java)
+
+    @Test
+    fun gardenActivityTest() {
+        val appCompatImageButton = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withId(R.id.appbar),
+                                                0)),
+                                1),
+                        isDisplayed()))
+        appCompatImageButton.perform(click())
+
+        val navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.navigation_view),
+                                        0)),
+                        2),
+                        isDisplayed()))
+        navigationMenuItemView.perform(click())
+
+        val textView = onView(
+                allOf(withId(R.id.plant_item_title), withText("Apple"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.plant_list),
+                                        0),
+                                1),
+                        isDisplayed()))
+        textView.check(matches(withText("Apple")))
+    }
+
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
